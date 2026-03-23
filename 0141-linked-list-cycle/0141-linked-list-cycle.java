148d5e16b2989b3cfdc3a8c7c9a6264b625b5e0e
Selvalakshmi-k <kselvalakshmi38@gmail.com>
2026-03-23 11:28:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -14,10 +14,11 @@ public class Solution {
         ListNode slow=head;
         ListNode fast=head;
         while(fast!=null && fast.next!=null){
-            if(slow.next==fast.next.next)
-            return true;
             slow=slow.next;
             fast=fast.next.next;
+            if(slow==fast)
+            return true;
+            
         }
         return false;
     }
