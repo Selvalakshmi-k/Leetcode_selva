5de53ca167cd458dd20073cd1d94d8e75d47d379
Selvalakshmi-k <kselvalakshmi38@gmail.com>
2026-04-12 10:01:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.java
+++ 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.java
@@ -7,21 +7,48 @@ class Solution {
 //             }
 //         }));
 //     }
-    static int findgcd(int a, int b){
-        if(b==0)
-            return a;
-        return findgcd(b,a%b);
+    // static int findgcd(int a, int b){
+    //     if(b==0)
+    //         return a;
+    //     return findgcd(b,a%b);
+    //     }
+    static boolean divide(int d,int[] numsDivide){
+        for(int i=0;i<numsDivide.length;i++){
+            if(numsDivide[i]%d!=0){
+                return false;
+            }
         }
+        return true;
+    }
         public int minOperations(int[] nums, int[] numsDivide) {
         Arrays.sort(nums);
-        int g=numsDivide[0];
-        for(int i=1;i<numsDivide.length;i++){
-            g=findgcd(g,numsDivide[i]);
-        }
+        // int g=numsDivide[0];
+        // for(int i=1;i<numsDivide.length;i++){
+        //     g=findgcd(g,numsDivide[i]);
+        // }
+        // for(int i=0;i<nums.length;i++){
+        //     if(g%nums[i]==0)
+        //     return i;
+        // }
+        // return -1;
+        int d=0;
+        Set<Integer> s=new HashSet<>();
         for(int i=0;i<nums.length;i++){
-            if(g%nums[i]==0)
-            return i;
+            if(s.contains(nums[i])){
+                d++;
+                continue;
+            }
+            s.add(nums[i]);
+            if(!divide(nums[i],numsDivide)){
+                d++;
+            }else{
+                break;
+            }
+            
         }
-        return -1;
+        if(d==nums.length){
+            return -1;
+        }
+        return d;
     }
 }
