7d780b16a110bcc48c0c266290dbd0d607b9ebca
Selvalakshmi-k <kselvalakshmi38@gmail.com>
2026-06-03 07:10:18 +0530

    Time: 4 ms (82.25%), Space: 46.5 MB (32.32%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] input1 =s.toCharArray();
+        char[] input2 =t.toCharArray();
+        Arrays.sort(input1);
+        Arrays.sort(input2);
+        if(s.length()!=t.length())
+        return false;
+        boolean flag=true;
+        for(int i=0;i<input1.length;i++){
+            if(input1[i]!=input2[i])
+            flag=false;
+        }
+        if(flag==false)
+        return false;
+        else
+        return true;
+        
+    }
+}
