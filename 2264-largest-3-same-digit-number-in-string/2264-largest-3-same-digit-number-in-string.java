05ccc17b8b4069d954ad00b4a4d75f4ddfd5466e
Selvalakshmi-k <kselvalakshmi38@gmail.com>
2026-08-20 18:38:18 +0530

    Time: 6 ms (24.49%), Space: 49.1 MB (19.07%) - LeetHub

---
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String largestGoodInteger(String num) {
+        char max=' ';
+        for(int i=0;i<num.length()-2;i++){
+            if((num.charAt(i)==num.charAt(i+1))&&
+            num.charAt(i)==num.charAt(i+2)){
+                max=(char)Math.max(max,num.charAt(i));
+            }
+        }
+        if(max==' '){
+            return "";
+        }
+        return ""+max+max+max;
+    }
+}
